9fe75b24f6241bc12043ace1d6617c7f41dba2f0
Diego <diego@mint.personal>
2023-08-12 15:30:05 +0200

    fixes

--- backend/telegram/api/types.go
+++ backend/telegram/api/types.go
@@ -1,53 +1,53 @@
-package api
+// package api
 
-type InputPeer struct {
-  _     'inputPeer'
-  user_id     number
-  access_hash     string
-}
+// type InputPeer struct {
+//   _     'inputPeer'
+//   user_id     number
+//   access_hash     string
+// }
 
-type InputUser struct {
-  _     'inputUser'
-  user_id     number
-  access_hash     string
-}
+// type InputUser struct {
+//   _     'inputUser'
+//   user_id     number
+//   access_hash     string
+// }
 
-type InputChannel struct {
-  _     'inputChannel'
-  channel_id     number
-  access_hash     string
-}
+// type InputChannel struct {
+//   _     'inputChannel'
+//   channel_id     number
+//   access_hash     string
+// }
 
-type InputMessageID struct {
-  _     'inputMessageID'
-  id     number
-}
+// type InputMessageID struct {
+//   _     'inputMessageID'
+//   id     number
+// }
 
-type MessagesGetMessages struct {
-  _     'messages.getMessages'
-  id     InputMessageID[]
-}
+// type MessagesGetMessages struct {
+//   _     'messages.getMessages'
+//   id     InputMessageID[]
+// }
 
-type MessagesGetMessagesResponse struct {
-  messages     Message[]
-}
+// type MessagesGetMessagesResponse struct {
+//   messages     Message[]
+// }
 
-type Message struct {
-  _     'message'
-  id     number
-  media     Media
-}
+// type Message struct {
+//   _     'message'
+//   id     number
+//   media     Media
+// }
 
-type Media struct {
-  _     'messageMediaDocument'
-  document     Document
-}
+// type Media struct {
+//   _     'messageMediaDocument'
+//   document     Document
+// }
 
-type Document struct {
-  _     'document'
-  id     string
-  access_hash     string
-  size     number
-  mime_type     string
-  file_reference     Uint8Array
-}
+// type Document struct {
+//   _     'document'
+//   id     string
+//   access_hash     string
+//   size     number
+//   mime_type     string
+//   file_reference     Uint8Array
+// }
