c5af630607415f653120915edb6ac0e779570f25
Diego <diego@mint.personal>
2023-08-12 15:16:35 +0200

    fixes

--- backend/telegram/api/types.go
+++ backend/telegram/api/types.go
@@ -1,53 +1,107 @@
-export namespace Api {
-  export type InputPeer = {
-    _: 'inputPeer'
-    user_id: number
-    access_hash: string
-  }
+package api
 
-  export type InputUser = {
-    _: 'inputUser'
-    user_id: number
-    access_hash: string
-  }
-
-  export type InputChannel = {
-    _: 'inputChannel'
-    channel_id: number
-    access_hash: string
-  }
-
-  export type InputMessageID = {
-    _: 'inputMessageID'
-    id: number
-  }
-
-  export type MessagesGetMessages = {
-    _: 'messages.getMessages'
-    id: InputMessageID[]
-  }
-
-  export type MessagesGetMessagesResponse = {
-    messages: Message[]
-  }
-
-  export type Message = {
-    _: 'message'
-    id: number
-    media: Media
-  }
-
-  export type Media = {
-    _: 'messageMediaDocument'
-    document: Document
-  }
-
-  export type Document = {
-    _: 'document'
-    id: string
-    access_hash: string
-    size: number
-    mime_type: string
-    file_reference: Uint8Array
-  }
+type InputPeer = {
+  _: 'inputPeer'
+  user_id: number
+  access_hash: string
 }
+
+type InputUser = {
+  _: 'inputUser'
+  user_id: number
+  access_hash: string
+}
+
+type InputChannel = {
+  _: 'inputChannel'
+  channel_id: number
+  access_hash: string
+}
+
+type InputMessageID = {
+  _: 'inputMessageID'
+  id: number
+}
+
+type MessagesGetMessages = {
+  _: 'messages.getMessages'
+  id: InputMessageID[]
+}
+
+type MessagesGetMessagesResponse = {
+  messages: Message[]
+}
+
+type Message = {
+  _: 'message'
+  id: number
+  media: Media
+}
+
+type Media = {
+  _: 'messageMediaDocument'
+  document: Document
+}
+
+type Document = {
+  _: 'document'
+  id: string
+  access_hash: string
+  size: number
+  mime_type: string
+  file_reference: Uint8Array
+}
+
+// export namespace Api {
+//   export type InputPeer = {
+//     _: 'inputPeer'
+//     user_id: number
+//     access_hash: string
+//   }
+
+//   export type InputUser = {
+//     _: 'inputUser'
+//     user_id: number
+//     access_hash: string
+//   }
+
+//   export type InputChannel = {
+//     _: 'inputChannel'
+//     channel_id: number
+//     access_hash: string
+//   }
+
+//   export type InputMessageID = {
+//     _: 'inputMessageID'
+//     id: number
+//   }
+
+//   export type MessagesGetMessages = {
+//     _: 'messages.getMessages'
+//     id: InputMessageID[]
+//   }
+
+//   export type MessagesGetMessagesResponse = {
+//     messages: Message[]
+//   }
+
+//   export type Message = {
+//     _: 'message'
+//     id: number
+//     media: Media
+//   }
+
+//   export type Media = {
+//     _: 'messageMediaDocument'
+//     document: Document
+//   }
+
+//   export type Document = {
+//     _: 'document'
+//     id: string
+//     access_hash: string
+//     size: number
+//     mime_type: string
+//     file_reference: Uint8Array
+//   }
+// }
